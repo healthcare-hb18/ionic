93c381a31835f5ea71b34d0ea14ecee5759bceaf
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2018-05-26 21:05:11 +0200

    added barcodeScanner from Dani

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BodyPage } from '../body/body';
-
+import { BarcodeScanner} from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,  public navParams: NavParams) {
+  results: {};
+
+  constructor(public navCtrl: NavController,  public navParams: NavParams, private barcode: BarcodeScanner) {
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad homePage');
+  }
+
+  async scanBarcode() {
+    this.results = await this.barcode.scan();
+
   }
 
   openBodyPage(){
